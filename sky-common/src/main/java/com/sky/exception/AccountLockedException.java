5319aa69cc640929df752f9fe4551cb836641e92
1900369667 <1900369667@qq.com>
2025-04-23 18:33:36 +0800

    账号被冻结异常

---
+++ sky-common/src/main/java/com/sky/exception/AccountLockedException.java
@@ -0,0 +1,10 @@
+package com.sky.exception;
+
+/**
+ * @author Canyon
+ */
+public class AccountLockedException extends BaseException {
+    public AccountLockedException(String msg){
+        super(msg);
+    }
+}
