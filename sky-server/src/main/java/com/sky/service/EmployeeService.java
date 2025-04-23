a63a50c24bb13fd878ad6baf0ce786b423fd8d65
1900369667 <1900369667@qq.com>
2025-04-23 18:34:31 +0800

    员工service接口

---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.sky.service;
+
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.entity.Employee;
+
+/**
+ * @author Canyon
+ */
+public interface EmployeeService {
+    /**
+     * 判断用户登录逻辑，返回用户信息或者抛出异常
+     * @param employeeLoginDTO 前端返回对象
+     * @return 用户登录实体
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+}
