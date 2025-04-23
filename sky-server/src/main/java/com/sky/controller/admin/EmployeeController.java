04b597fce2e82d8be94fb1e5173cdeab5fad7aed
1900369667 <1900369667@qq.com>
2025-04-23 18:34:15 +0800

    员工controller层，后续补充完整用md5加密密码

---
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -0,0 +1,67 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.entity.Employee;
+import com.sky.properties.JwtProperties;
+import com.sky.service.EmployeeService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.EmployeeLoginVO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sky.result.Result;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Canyon
+ */
+@Slf4j
+@RestController
+@RequestMapping("/admin/employee")
+@RequiredArgsConstructor
+
+
+public class EmployeeController {
+
+
+    private final EmployeeService employeeService;
+    private final JwtProperties jwtProperties;
+
+    /**
+     * MGZ TODO 2025/4/23：未实现md5加密
+     *
+     * @param employeeLoginDTO 前端数据传输对象
+     * @return {@link Result }<{@link EmployeeLoginVO }>
+     */
+    @PostMapping("/login")
+    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO){
+        log.info("员工登录：{}", employeeLoginDTO);
+        Employee employee = employeeService.login(employeeLoginDTO);
+
+        //登录成功后，生成jwt令牌
+        Map<String, Object> claims = new HashMap<>(2);
+        claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
+        claims.put(JwtClaimsConstant.USERNAME,employee.getUsername());
+
+        String token = JwtUtil.createJwt(
+                jwtProperties.getAdminSecretKey(),
+                jwtProperties.getAdminTtl(),
+                claims);
+
+        EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
+                .id(employee.getId())
+                .userName(employee.getUsername())
+                .name(employee.getName())
+                .token(token)
+                .build();
+
+        return Result.success(employeeLoginVO);
+    }
+}
