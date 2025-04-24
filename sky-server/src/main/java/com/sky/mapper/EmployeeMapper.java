6ec8f39327173e6260b256533a5623fe8b404859
1900369667 <1900369667@qq.com>
2025-04-24 15:50:53 +0800

    新增添加员工sql

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -14,4 +15,23 @@ public interface EmployeeMapper {
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
+
+    /**
+     * 添加新员工
+     * @param employee 员工实体
+     */
+    @Insert("INSERT INTO " +
+            "employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
+            "values " +
+            "(#{name}," +
+            "#{username}," +
+            "#{password}," +
+            "#{phone}," +
+            "#{sex}," +
+            "#{idNumber}," +
+            "#{createTime}," +
+            "#{updateTime}," +
+            "#{createUser}," +
+            "#{updateUser})")
+    void employeeAdd(Employee employee);
 }
