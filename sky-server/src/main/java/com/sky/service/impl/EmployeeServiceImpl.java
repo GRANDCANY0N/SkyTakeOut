1cab82d3f4382d29815bb9aeca2ecb9e21f129a8
1900369667 <1900369667@qq.com>
2025-04-24 15:53:39 +0800

    新增添加员工实现

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,27 +1,31 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeAddDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import com.sky.exception.AccountExistException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.sky.exception.AccountLockedException;
 import org.springframework.util.DigestUtils;
 
+import java.time.LocalDateTime;
 
+@RequiredArgsConstructor
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-
-    @Autowired
-    private EmployeeMapper employeeMapper;
-
+    private final EmployeeMapper employeeMapper;
 
     /**
      * @param employeeLoginDTO 前端数据传输对象
@@ -46,5 +50,33 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         return employee;
+    }
+
+    /**
+     * 1、判断是否存在该账号
+     * 2、new一个员工对象
+     * 3、将DTO的值放入
+     * 4、将时间以及密码等默认值放入并加密
+     */
+    @Override
+    public void employeeAdd(EmployeeAddDTO employeeAddDTO) {
+        String username = employeeAddDTO.getUsername();
+        if(employeeMapper.getByUsername(username)!=null){
+            throw new AccountExistException(username + MessageConstant.ACCOUNT_EXISTS);
+        }
+        Employee employee = new Employee();
+
+        //复制DTO到对象
+        BeanUtils.copyProperties(employeeAddDTO,employee);
+
+        employee.setStatus(StatusConstant.ENABLE);
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.PASSWORD.getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        employeeMapper.employeeAdd(employee);
     }
 }
